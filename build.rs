fb0a64372d28efd3e86a348d33dab8eab4589fab
gimura <gimura0001@gmail.com>
2024-07-26 02:18:52 +0300

    added build scripts to run preprocessor

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::{fs::File, io::Write};
+
+use fast_voxel_perprocessor::preprocess_dir;
+
+fn main() -> Result<(), ()> {
+    println!("cargo::rerun-if-changed=src/shaders");
+
+    let preprocessed = preprocess_dir("src/shaders".to_string());
+
+    match preprocessed {
+        Ok(source) => {
+            let mut file = File::create("src/compiled.wgsl")
+                .expect("Error to open file!");
+
+            file.write_all(source.as_bytes())
+                .expect("Error to write file!");
+
+            Ok(())
+        },
+        Err(error) => {
+            panic!("{}", error.to_string());
+        },
+    }
+}
